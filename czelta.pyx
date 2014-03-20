1d031c9791208a945e56f5d0fe32410530bb1f99
Martin Quarda <hinogary@gmail.com>
2014-03-20 06:36:20 +0100

    merge repair

--- czelta.pyx
+++ czelta.pyx
@@ -1,3 +1,98 @@
+<<<<<<< HEAD
+=======
+#author: Martin Quarda
+from libcpp.string cimport string
+from libcpp.vector cimport vector
+
+cdef extern from "station.h" nogil:
+    cppclass Station:
+        Station() except +
+        Station(int ID) except +
+        inline int id()
+        inline char* name()
+        short* lastTDCCorrect()
+        short* TDCCorrect(int timestamp)
+        double* detectorPosition()
+        double* GPSPosition()
+        
+        double distanceTo(Station& st)
+        
+        void setName(char* name)
+        void setGPSPosition(double latitude, double longitude, double height)
+        void setDetectorPosition(double x1, double y1, double x2, double y2)
+        void clearTDCCorrect(int capacity)
+        void pushTDCCorrect(int fr, short tdc0, short tdc1, short tdc2)
+        void pushTDCCorrect(string fr, short tdc0, short tdc1, short tdc2)
+        void pushFileName(string name)
+        
+        
+ctypedef Station* p_Station
+cdef extern from "station.h" namespace "Station" nogil:
+    bint addStation(Station)
+    Station& getStation(int)
+    Station& getStation(string)
+    vector[p_Station] getStations()
+
+
+cdef extern from "event_reader.h" nogil:
+    cppclass EventReader:
+        EventReader() except +
+        setStation(int id)
+        inline int numberOfEvents()
+        bint loadDatFile(char* filename)
+        bint loadTxtFile(char* filename)
+        bint saveDatFile(char* filename)
+        bint saveTxtFile(char* filename)
+        inline Event& item(int index)
+        inline Event& item(int run, int index)
+        
+        #filters
+        int filterCalibs()
+        inline int filterMaxTDC()
+        inline int filterMaxADC()
+        inline int filterMinADC()
+        
+        inline int numberOfRuns()
+        inline int numberOfEvents(int run)
+        
+cdef extern from "event_reader.h" namespace "EventReader" nogil:
+    static void setFilesDirectory(string dir)
+    inline static string getFilesDirectory()
+
+        
+cdef extern from "event.h" nogil:
+    cppclass Event:
+        Event() except +
+        Event(int timestamp,double last_secod, int TDC0, int TDC1, int TDC2, int ADC0, int ADC1, int ADC2, int t0, int t1, int t2, int tCrateRaw, bint calibration, bint run) except +
+        Event(const Event& orig)
+        inline int timestamp()
+        inline double last_second()
+        inline double time_since_second()
+        inline long tenthOfNSTimestamp()
+        inline short* TDC()
+        inline short TDC0()
+        inline short TDC1()
+        inline short TDC2()
+        inline short* ADC()
+        inline short ADC0()
+        inline short ADC1()
+        inline short ADC2()
+        inline short* tempsRaw()
+        inline float* temps()
+        inline short t0raw()
+        inline short t1raw()
+        inline short t2raw()
+        inline float t0()
+        inline float t1()
+        inline float t2()
+        inline short tCrateRaw()
+        inline float tCrate()
+        inline bint isCalib()
+        string toString()
+
+
+
+>>>>>>> d7539e1... Update czelta.pyx
 import datetime
 import json
 
