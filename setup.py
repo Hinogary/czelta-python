f19c09b9caca0ae495cd457862edb050cb87cdfe
Martin Quarda <hinogary@gmail.com>
2014-03-23 11:00:28 +0100

    now can be compiled without Cython

--- setup.py
+++ setup.py
@@ -2,15 +2,43 @@
 # vim: set fileencoding=UTF-8 :
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
-ext_modules = [Extension("czelta", ["czelta.pyx","event.cpp","event_reader.cpp","common_func.cpp","station.cpp","coincidence.cpp"],
-            language="c++",
-            extra_compile_args=["-std=c++0x"]
-)]
+name = 'czelta'
+version = '0.1'
+description = ''
+author = 'Martin Quarda'
+author_email = 'hinogary@gmail.com'
+url = 'http://czelta.quarda.cz/'
 
-setup(
-  name = "czelta",
-  cmdclass = {"build_ext": build_ext},
-  ext_modules = ext_modules,
-)
+try:
+    from Cython.Distutils import build_ext
+
+    ext_modules = [Extension('czelta', ['czelta.pyx','event.cpp','event_reader.cpp','common_func.cpp','station.cpp','coincidence.cpp'],
+                language='c++',
+                extra_compile_args=['-std=c++0x']
+    )]
+    setup(
+      name = name,
+      version = version,
+      description = description,
+      author = author,
+      author_email = author_email,
+      url = url,
+      cmdclass = {'build_ext': build_ext},
+      ext_modules = ext_modules
+    )
+except ImportError:
+    print('Cython can\'t import, try to compile module with precompiled czelta.pyx')
+    ext_modules = [Extension('czelta', ['czelta.cpp','event.cpp','event_reader.cpp','common_func.cpp','station.cpp','coincidence.cpp'],
+                language='c++',
+                extra_compile_args=['-std=c++0x']
+    )]
+    setup(
+      name = name,
+      version = version,
+      description = description,
+      author = author,
+      author_email = author_email,
+      url = url,
+      ext_modules = ext_modules
+    )
