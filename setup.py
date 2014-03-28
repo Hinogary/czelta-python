b47cfb0c729fa6dcdb294451a6b5076098e87377
Martin Quarda <hinogary@gmail.com>
2014-03-28 14:41:24 +0100

    totally remade setup.py

--- setup.py
+++ setup.py
@@ -3,42 +3,31 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-name = 'czelta'
-version = '0.1'
-description = ''
-author = 'Martin Quarda'
-author_email = 'hinogary@gmail.com'
-url = 'http://czelta.quarda.cz/'
-
 try:
-    from Cython.Distutils import build_ext
-
-    ext_modules = [Extension('czelta', ['czelta.pyx','event.cpp','event_reader.cpp','common_func.cpp','station.cpp','coincidence.cpp'],
-                language='c++',
-                extra_compile_args=['-std=c++0x']
-    )]
-    setup(
-      name = name,
-      version = version,
-      description = description,
-      author = author,
-      author_email = author_email,
-      url = url,
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = ext_modules
-    )
+    from Cython.Build import cythonize
+    USE_CYTHON = True
 except ImportError:
-    print('Cython can\'t import, try to compile module with precompiled czelta.pyx')
-    ext_modules = [Extension('czelta', ['czelta.cpp','event.cpp','event_reader.cpp','common_func.cpp','station.cpp','coincidence.cpp'],
+    USE_CYTHON = False
+
+ext_modules = [Extension('czelta', ['czelta.pyx' if USE_CYTHON else 'czelta.cpp',
+                                    'event.cpp',
+                                    'event_reader.cpp',
+                                    'common_func.cpp',
+                                    'station.cpp',
+                                    'coincidence.cpp'],
                 language='c++',
                 extra_compile_args=['-std=c++0x']
     )]
-    setup(
-      name = name,
-      version = version,
-      description = description,
-      author = author,
-      author_email = author_email,
-      url = url,
-      ext_modules = ext_modules
-    )
+if USE_CYTHON:
+    ext_modules = cythonize(ext_modules)
+
+setup(
+  name = 'czelta',
+  version = '0.1',
+  description = 'Analysis libraty for project CZELTA (http://czelta.utef.cvut.cz/)',
+  author = 'Martin Quarda',
+  author_email = 'hinogary@gmail.com',
+  url = 'http://czelta.quarda.cz/',
+  long_description = open('README.md').read(),
+  ext_modules = ext_modules
+)
