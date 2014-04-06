bb998492217c74cb200b0083e47de4494920a6c8
Martin Quarda <hinogary@gmail.com>
2014-04-06 20:56:08 +0200

    removed things copied from data_converter

--- miniaplications/coincidence.py
+++ miniaplications/coincidence.py
@@ -11,53 +11,6 @@ for i in range(len(sys.argv)):
 
 class MainWindow(coincidence_ui.Ui_MainWindow):
 
-    def select_data(self):
-        fname = str(QtGui.QFileDialog.getOpenFileName(self.mainwindow, 'Open file', 
-            self.last_directory if self.last_directory else os.path.expanduser('~'),"Shower data (*.dat *.txt)"))
-        if fname=='':
-            return
-        self.last_directory = os.path.dirname(fname)
-        self.path_data.setText(fname)
-        
-    def convert_data(self):
-        format = "dat" if self.radio_dat_file.isChecked() else "txt"
-        fname = QtGui.QFileDialog.getSaveFileName(self.mainwindow, 'Save file', self.last_directory, "Shower data (*.%s)"%format)
-        if fname=='':
-            return
-        if not fname[-4:] in (".txt",".dat"):
-            fname += "."+format
-
-        event_reader = czelta.event_reader()
-        event_reader.load(str(self.path_data.displayText()))
-        
-        if self.filter_calibrations.isChecked():
-            event_reader.filter_calibrations()
-            
-        if self.filter_maximum_TDC.isChecked():
-            event_reader.filter_maximum_TDC()
-            
-        if self.filter_maximum_ADC.isChecked():
-            event_reader.filter_maximum_ADC()
-        
-        if self.filter_minimum_ADC.isChecked():
-            event_reader.filter_minimum_ADC()
-        try:
-            if event_reader.save(str(fname), not self.filter_x_events.isChecked()):
-                raise IOError
-        except IOError:
-            QtGui.QMessageBox.warning(self.mainwindow,
-                _translate("MainWindow", "error_title", None),
-                _translate("MainWindow", "error_cant_save", None))
-            return
-        except NotImplementedError:
-            QtGui.QMessageBox.warning(self.mainwindow,
-                _translate("MainWindow", "error_title", None), 
-                _translate("MainWindow", "error_cant_save_bad_suffix", None))
-            return
-        QtGui.QMessageBox.information(self.mainwindow,
-            _translate("MainWindow", "success", None),
-            _translate("MainWindow", "file_saved", None))
-
     def __init__(self):
         self.last_directory = None
         self.mainwindow = QtGui.QMainWindow()
