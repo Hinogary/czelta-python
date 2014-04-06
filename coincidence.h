d0526a5b28c22bded44e78f9809d6f6795e1b32a
Martin Quarda <hinogary@gmail.com>
2014-04-06 09:59:16 +0200

    added save_events parameter

--- coincidence.h
+++ coincidence.h
@@ -5,12 +5,14 @@
 #define COINCIDENCE_H
 #include "event_reader.h"
 #include "event.h"
+#include "station.h"
 class Coincidence
 {
 public:
     Coincidence();
-    void calc(double limit);
+    void calc(double limit, bool save_events = true);
     EventReader *readers[2];
+    Station *stations[2];
     vector<double> delta;
     vector<Event> events[2];
     double limit;
