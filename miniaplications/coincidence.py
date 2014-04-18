74c414b739ef3586d1c3dc33d27de443246f0071
Martin Quarda <hinogary@gmail.com>
2014-04-18 17:18:01 +0200

    updated mini app to triple coincidences

--- miniaplications/coincidence.py
+++ miniaplications/coincidence.py
@@ -19,24 +19,35 @@ class MainWindow(coincidence_ui.Ui_MainWindow):
         
     def select_2_data(self):
         self.select_data(2)
+        
+    def select_3_data(self):
+        self.select_data(3)
     
     def data_1_changed(self):
         self.ers_changed[0] = True
     
     def data_2_changed(self):
         self.ers_changed[1] = True
-    
+        
+    def data_3_changed(self):
+        self.ers_changed[2] = True
+        
     def select_data(self, station):
         fname = str(QtGui.QFileDialog.getOpenFileName(self.mainwindow, 'Open file', 
             self.last_directory if self.last_directory else os.path.expanduser('~'),"Shower data (*.dat *.txt)"))
         if fname=='':
             return
         self.last_directory = os.path.dirname(fname)
-        data = self.data_1 if station==1 else self.data_2
+        data = self.data_1 if station==1 else self.data_2 if station==2 else self.data_3
         data.setText(fname)
+
+    def set_triple(self, val):
+        self.st_g_3.setEnabled(val)
 
     def find_coincidences(self):
         save_events = self.save_events.isChecked()
+        triple = self.triple_coin.isChecked()
+        print(triple)
         try:
             if self.ers_changed[0]:
                 self.ers[0].load(str(self.data_1.text()))
@@ -44,7 +55,7 @@ class MainWindow(coincidence_ui.Ui_MainWindow):
         except IOError:
             QtGui.QMessageBox.warning(self.mainwindow,
                 _translate("MainWindow", "error_title", None), 
-                _translate("MainWindow", "error_cant_open_data_1", None))
+                _translate("MainWindow", "error_cant_open_data_%1", None).arg(1))
             return
         try:
             if self.ers_changed[1]:
@@ -53,8 +64,18 @@ class MainWindow(coincidence_ui.Ui_MainWindow):
         except IOError:
             QtGui.QMessageBox.warning(self.mainwindow,
                 _translate("MainWindow", "error_title", None), 
-                _translate("MainWindow", "error_cant_open_data_2", None))
+                _translate("MainWindow", "error_cant_open_data_%1", None).arg(2))
             return
+        if triple:
+            try:
+                if self.ers_changed[2]:
+                    self.ers[2].load(str(self.data_2.text()))
+                    self.ers_changed[2] = False
+            except IOError:
+                QtGui.QMessageBox.warning(self.mainwindow,
+                    _translate("MainWindow", "error_title", None), 
+                    _translate("MainWindow", "error_cant_open_data_%1", None).arg(3))
+                return
         if save_events and self.delta_spin.value()*1e-6 > 0.1:
             answer = QtGui.QMessageBox.question(self.mainwindow, 
                 _translate("MainWindow", "question_limit", None), 
@@ -63,24 +84,36 @@ class MainWindow(coincidence_ui.Ui_MainWindow):
             if answer!=QtGui.QMessageBox.Yes:
                 self.save_events.setChecked(False)
                 save_events = False
-        c = czelta.coincidence(self.ers, self.delta_spin.value()*1e-6, save_events)
+        if triple:
+            ers = self.ers[0], self.ers[1], self.ers[2]
+        else:
+            ers = self.ers[0], self.ers[1]
+        c = czelta.coincidence(ers, self.delta_spin.value()*1e-6, save_events = save_events)
+        print(ers)
+        print(self.delta_spin.value()*1e-6)
+        print(c.overlap_measure_time)
         self.number_of_coincidences.setText("%d"%len(c))
         self.medium_value.setText("%.2f"%c.expected_value)
         self.percents.setText("%.2f %%"%(c.chance*100))
-        self.lenght_of_measuring.setText("%.2f %s"%(float(c.overlap_measure_time)/86400, _translate("MainWindow","days",None)))
+        self.lenght_of_measuring.setText("%.2f %s"%(c.overlap_measure_time/86400, _translate("MainWindow","days",None)))
         self.all_events_0.setText("%d"%c.overlap_normal_events[0])
         self.all_events_1.setText("%d"%c.overlap_normal_events[1])
         if save_events:
             self.coincidence_text_edit.setPlainText("")
             for coin in c:
-                self.coincidence_text_edit.insertPlainText(u"%.2f μs\n%s\n%s\n"%(coin[0]*1e6,str(coin[1]),str(coin[2])))
+                if triple:
+                    text = u"%.2f μs\n%s\n%s\n%s\n"%(coin[0]*1e6,str(coin[1]),str(coin[2]),str(coin[3]))
+                else:
+                    text = u"%.2f μs\n%s\n%s\n"%(coin[0]*1e6,str(coin[1]),str(coin[2]))
+                self.coincidence_text_edit.insertPlainText(text)
             self.coincidence_text_edit.show()
         else:
             self.coincidence_text_edit.hide()
+        self.coincidence_text_edit.setEnabled(True);
     
     def __init__(self):
-        self.ers = czelta.event_reader(), czelta.event_reader()
-        self.ers_changed = [True, True]
+        self.ers = czelta.event_reader(), czelta.event_reader(), czelta.event_reader()
+        self.ers_changed = [True, True, True]
         self.last_directory = None
         self.mainwindow = QtGui.QMainWindow()
         self.setupUi(self.mainwindow)
@@ -88,9 +121,13 @@ class MainWindow(coincidence_ui.Ui_MainWindow):
         self.statusBar.showMessage(u"© 2013-2014 Martin Quarda")
         QtCore.QObject.connect(self.data_1_select, QtCore.SIGNAL('clicked()'), self.select_1_data)
         QtCore.QObject.connect(self.data_2_select, QtCore.SIGNAL('clicked()'), self.select_2_data)
+        QtCore.QObject.connect(self.data_3_select, QtCore.SIGNAL('clicked()'), self.select_3_data)
         QtCore.QObject.connect(self.calc_coincidences, QtCore.SIGNAL('clicked()'), self.find_coincidences)
+        QtCore.QObject.connect(self.triple_coin, QtCore.SIGNAL('toggled(bool)'), self.set_triple)
+        
         QtCore.QObject.connect(self.data_1, QtCore.SIGNAL('textChanged(const QString&)'), self.data_1_changed)
         QtCore.QObject.connect(self.data_2, QtCore.SIGNAL('textChanged(const QString&)'), self.data_2_changed)
+        QtCore.QObject.connect(self.data_3, QtCore.SIGNAL('textChanged(const QString&)'), self.data_2_changed)
 
 def main():
     app = QtGui.QApplication(sys.argv)
--- miniaplications/coincidence_cs.ts
+++ miniaplications/coincidence_cs.ts
@@ -4,94 +4,90 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="coincidence.ui" line="14"/>
         <source>coincidence_title</source>
-        <translation>Dvojkoincidence</translation>
+        <translation>Koincidence stanic</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="21"/>
         <source>1_station</source>
         <translation>1. Stanice</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="27"/>
-        <location filename="coincidence.ui" line="53"/>
-        <source>data_path</source>
-        <translation>Cesta k datům spršek:</translation>
-    </message>
-    <message>
-        <location filename="coincidence.ui" line="37"/>
-        <location filename="coincidence.ui" line="63"/>
-        <source>...</source>
-        <translation>...</translation>
-    </message>
-    <message>
-        <location filename="coincidence.ui" line="47"/>
         <source>2_station</source>
         <translation>2. Stanice</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="73"/>
+        <source>3_station</source>
+        <translation>3. Stanice</translation>
+    </message>
+    <message>
+        <source>double_coin</source>
+        <translation>dvojkoincidence</translation>
+    </message>
+    <message>
+        <source>triple_coin</source>
+        <translation>trojkoincidence</translation>
+    </message>
+    <message>
+        <source>data_path</source>
+        <translation>Cesta k datům spršek:</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
         <source>parameters_coincidences</source>
         <translation>Parametry hledání koincidencí</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="79"/>
         <source>max_diff</source>
         <translation>Maximální rozdíl mezi jednotlivýma sprškama:</translation>
     </message>
     <message utf8="true">
-        <location filename="coincidence.ui" line="86"/>
         <source> μs</source>
         <translation> μs</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="105"/>
         <source>save_events</source>
         <translation>Ukládat eventy</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="115"/>
         <source>find_coincidence</source>
         <translation>Najít koincidence</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="131"/>
         <source>result</source>
         <translation>Výsledky</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="140"/>
         <source>number_of_coincidnces</source>
         <translation>Počet koincidencí:</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="154"/>
         <source>total_measure_time</source>
         <translation>Celková doba společného měření</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="168"/>
         <source>sum_1_station</source>
         <translation>Celkový počet spršek na 1. stanici:</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="182"/>
         <source>sum_2_station</source>
         <translation>Celkový počet spršek na 2. stanici:</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="232"/>
+        <source>sum_3_station</source>
+        <translation>Celkový počet spršek na 3. stanici:</translation>
+    </message>
+    <message>
         <source>medium_value</source>
         <translation>Střední předpokládaný počet koincidencí:</translation>
     </message>
     <message utf8="true">
-        <location filename="coincidence.ui" line="246"/>
         <source>chance_tooltip</source>
         <translation>Pravděpodobnost, že ve zvoleném časovém intervalu bude počet spršek zaregistrovaných, nebo i vyšší.</translation>
     </message>
     <message>
-        <location filename="coincidence.ui" line="249"/>
         <source>chance</source>
         <translation>Pravděpodobnost:</translation>
     </message>
@@ -100,12 +96,8 @@
         <translation>Chyba!</translation>
     </message>
     <message>
-        <source>error_cant_open_data_1</source>
-        <translation>Chyba - nelze otevřít data 1. stanice!</translation>
-    </message>
-    <message>
-        <source>error_cant_open_data_2</source>
-        <translation>Chyba - nelue otevřít data 2. stanice!</translation>
+        <source>error_cant_open_data_%1</source>
+        <translation>Chyba - nelze otevřít data %1. stanice!</translation>
     </message>
     <message>
         <source>days</source>
