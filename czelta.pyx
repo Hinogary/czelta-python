de7ad7a5cd735177a718538c1bbdda42965a6c28
Martin Quarda <hinogary@gmail.com>
2014-04-19 12:32:12 +0200

    added spaces

--- czelta.pyx
+++ czelta.pyx
@@ -28,16 +28,20 @@ cdef class station:
             self.st = &getStation(<string>station.encode(system_encoding))
         if self.st.id()==0:
             raise RuntimeError("Station not exist, have you loaded config file?")
+            
     cpdef int id(self):
         "Return `station id`, probably same as on czelta website."
         return self.st.id()
+        
     cpdef name(self):
         "Return code name of station. Example: ``\'praha_utef\'``, ``\'pardubice_gd\'`` or similar."
         return str((<char*>self.st.name()).decode(system_encoding))
+        
     cpdef detector_position(self):
         "Return position of detectors in format ``(x1, y1, x2, y2)`` where ``x1`` and ``y1`` are relative position of detector 1 to detector 0. ``x2`` and ``y2`` are relative position of detector 2 to detector 0. All values are in metres."
         cdef float* dp = self.st.detectorPosition()
         return (dp[0], dp[1], dp[2], dp[3])
+        
     cpdef distance_to(self, station other_station):
         "Calculate distance to other station using haversine method. The return number is in kilometres."
         cdef double distance = self.st.distanceTo(other_station.st[0])
@@ -45,12 +49,14 @@ cdef class station:
             return None
         else:
             return distance
+            
     cpdef gps_position(self):
         "Returns GPS position of station. Return ``(latitude, longitude)`` or ``None`` if gps_position is not defined."
         cdef double* gp = self.st.GPSPosition()
         if gp[0]==0 and gp[1]==0:
             return None
         return (gp[0], gp[1], gp[2])
+        
     @staticmethod
     def load(file = None):
         "Loads configuration file of stations. Must be called if you want to work with ``czelta.station``."
@@ -108,50 +114,62 @@ cdef class event:
     "Basic czelta class for holding information about events. This time is imposible to create own event"
     def __init__(self):
         pass
+        
     def __str__(self):
         return self.e.toString().decode(system_encoding)
+        
     cdef void set(self, Event e):
         self.e = e
+        
     property timestamp:
         "timestamp of event, fastest way to get time of event."
         def __get__(self):
             return self.e.timestamp()
+            
     property datetime:
         "Return python `datetime <http://docs.python.org/2/library/datetime.html>`_ object. All times with Czelta is in UTC."
         def __get__(self):
             return datetime.datetime.utcfromtimestamp(self.e.timestamp())
+            
     property time_since_second:
         "Return time elapsed since last second (0-0.999999... sec)."
         def __get__(self):
-            return self.e.time_since_second();
+            return self.e.time_since_second()
+            
     property ADC:
         "Relative energy absorbed in each detector. Probably not comparable along different stations. Minimum value is 0 and Maximum is 2047. If it is 2047 it shloud be more."
         def __get__(self):
             cdef short* adc = self.e.ADC()
             return (adc[0], adc[1], adc[2])
+            
     property TDC:
         "Relative time of activation each detector. TDC*25/1e12 = sec. Format: ``(TDC0, TDC1, TDC2)``."
         def __get__(self):
             cdef short* tdc = self.e.TDC()
             return (tdc[0], tdc[1], tdc[2])
+            
     property TDC_corrected:
         "Relative time of activation each detector. Corrected and can be used to calculate diraction. Correction options are in `config_data.JSON`. TDC*25/1e12 = sec."
         def __get__(self):
             cdef short* tdc = self.e.TDCCorrected()
             return (tdc[0], tdc[1], tdc[2])
+            
     property temps_detector:
         "Return 3 temps of each detector in time of event."
         def __get__(self):
             cdef float* temps = self.e.temps()
             return (temps[0], temps[1], temps[2])
+            
     property temp_crate:
         "Return Temperature in crate in time of event."
         def __get__(self):
             return self.e.tCrate()
+            
     property calibration:
         "Calibration events are events actived by LED diod in each detectors."
         def __get__(self):
             return self.e.isCalib()
+            
     property AH_direction:
         "Return ``(horizon, azimuth)`` direction of shower. Azimuth is from south clockwise. Both values are in Degres. Must have loaded info about stations and set station for ``event``/``event_reader``"
         def __get__(self):
@@ -195,8 +213,10 @@ cdef class coincidence:
                 st_id = (<event_reader>event_readers[st]).er.getStation();
                 self.c.stations[st] = st_id
         self.c.calc(max_difference, save_events)
+        
     def __len__(self):
         return self.c.numberOfCoincidences
+        
     def __getitem__(self, index):
         cdef int i
         if type(index) != int:
@@ -226,24 +246,29 @@ cdef class coincidence:
                 return rtn
         else:
             return (self.c.delta[i],)
+            
     def __iter__(self):
         self.i = -1
         return self
+        
     def __next__(self):
         self.i += 1
         if self.i < self.c.numberOfCoincidences:
             return self[self.i]
         else:
             raise StopIteration
+            
     property delta:
         def __get__(self):
             return list(self.c.delta)
+            
     property stations:
         def __get__(self):
             if self.c.n==2:
                 return station(self.c.stations[0]), station(self.c.stations[1])
             else:
                 return station(self.c.stations[0]), station(self.c.stations[1]), station(self.c.stations[2])
+                
     property events:
         def __get__(self):
             cdef Event e
@@ -260,27 +285,34 @@ cdef class coincidence:
                     ev.set(e)
                     rtn[i].append(ev)
             return rtn
+            
     property max_difference:
         def __get__(self):
             return self.c.limit
+            
     property number_of_coincidences:
         def __get__(self):
             return self.c.numberOfCoincidences
+            
     property expected_value:
         def __get__(self):
             return self.c.medium_value
+            
     property chance:
         def __get__(self):
             return self.c.chance
+            
     property overlap_measure_time:
         def __get__(self):
             return self.c.overlap.measureTime;
+            
     property overlap_normal_events:
         def __get__(self):
             if self.c.n==2:
                 return (self.c.overlap.normal_events[0], self.c.overlap.normal_events[1])
             else:
                 return (self.c.overlap.normal_events[0], self.c.overlap.normal_events[1], self.c.overlap.normal_events[2])
+                
     property overlap_calibration_events:
         def __get__(self):
             if self.c.n==2:
@@ -318,8 +350,10 @@ cdef class event_reader:
     def __init__(self, str path = ""):
         if(len(path)!=0):
             self.load(path)
+            
     def __len__(self):
         return self.er.numberOfEvents()
+        
     def __getitem__(self, i):
         cdef int ii, start, stop
         if type(i)==slice:
@@ -357,21 +391,26 @@ cdef class event_reader:
             e = event()
             e.set(self.er.item(ii))
             return e
+            
     def __iter__(self):
         self.i = -1
         return self
+        
     def __next__(self):
         self.i+=1
         if self.i < self.er.numberOfEvents():
             return self[self.i]
         else:
             raise StopIteration
+            
     cpdef run(self, int run_id):
         "Return iterable object containing all events in run"
         return event_reader_run(self, run_id)
+        
     cpdef runs(self):
         "Return iterable object containing all runs."
         return event_reader_runs(self)
+        
     cpdef load(self, path_to_file):
         "Load events from file. This delete all current events and tries to load events from file"
         if path_to_file == '':  
@@ -385,7 +424,7 @@ cdef class event_reader:
         cdef vector[p_Station] stations = getStations()
         for st in stations:
             try:
-                d = path_to_file.index(st.name().decode(system_encoding))
+                d = path_to_file.lower().index(st.name().decode(system_encoding))
             except ValueError:
                 continue
             if d>st_index:
@@ -403,6 +442,7 @@ cdef class event_reader:
                 raise IOError("can't open or read file: "+path_to_file)
         else:
             raise NotImplementedError("path must be a file with .txt or .dat")
+            
     cpdef save(self, path_to_file, bint x_events = True):
         "Save events to file. Can save txt or dat(binary files same as from website)."
         bytes_path = path_to_file.encode(system_encoding)
@@ -414,6 +454,7 @@ cdef class event_reader:
                 raise IOError("can't write file: "+path_to_file)
         else:
             raise NotImplementedError("path must be a file with .txt or .dat")
+            
     cpdef set_station(self, object st):
         "Set station for event_reader. Station is also set for all current events."
         cdef int _id
@@ -426,23 +467,29 @@ cdef class event_reader:
         else:
             raise ValueError("Unknown type of station")
         self.er.setStation(_id)
+        
     cpdef int number_of_events(self, int run = -1):
         "Return number of events in ``event_reader``. Same result have ``len(event_reader)``."
         if(run==-1):
             return self.er.numberOfEvents()
         else:
             return self.er.numberOfEvents(run)
+            
     cpdef int number_of_runs(self):
         "Return number of runs. Same result have ``len(event_reader.runs())``."
         return self.er.numberOfRuns()
+        
     cpdef int measure_length(self):
         return self.er.measurelength()
+        
     cdef Event c_item(self, int i):
         return self.er.item(i)
+        
     cpdef event item(self, int i):
         e = event()
         e.set(self.er.item(i))
         return e
+        
     #filters
     cpdef int filter(self, filter_func):
         "Custom-filter function. As parameter give a function, which is ready to be called with parameter event object, which return True if you want remove event and False if you want let event in event_reader."
@@ -462,18 +509,23 @@ cdef class event_reader:
         _filter_func_object = filter_func
         _filter_func_event = event()
         return self.er.filter(&_filter_func)
+        
     cpdef int filter_calibrations(self):
         "Predefined fast filter. Filter all events marked as calibration."
         return self.er.filterCalibs()
+        
     cpdef int filter_maximum_TDC(self):
         "Predefined fast filter. Filter all events which have at least one TDC chanel equal maximum value (4095). Events with maximum value have bad measured TDC and sky direction can't be determined right."
         return self.er.filterMaxTDC()
+        
     cpdef int filter_maximum_ADC(self):
         "Predefined fast filter. Filter all events which have at least one ADC(energy) channel equal maximum value(2047)." 
         return self.er.filterMaxADC()
+        
     cpdef int filter_minimum_ADC(self):
         "Predefined fast filter. Filter all events which have at least one ADC(energy) channel equal zero (Not measured)."
         return self.er.filterMinADC()
+        
 #filter_func wrapper
 cdef bint _filter_func(Event& e):
     global _filter_func_event
@@ -490,19 +542,24 @@ cdef class event_reader_runs:
     "Iteratable class for runs of ``czelta.event_reader``."
     def __init__(self, event_reader reader):
         self.er = reader
+        
     def __str__(self):
         return "<czelta.event_reader_runs object with %i runs from event_reader: %s>"%(len(self),str(self.er))
+        
     def __iter__(self):
         self.i = -1
         return self
+        
     def __next__(self):
         self.i+=1
         if self.i < self.er.er.numberOfRuns():
             return self[self.i]
         else:
             raise StopIteration
+            
     def __len__(self):
         return self.er.er.numberOfRuns()
+        
     def __getitem__(self, i):
         cdef int ii, start, stop, step
         if type(i)==slice:
@@ -530,17 +587,21 @@ cdef class event_reader_run:
     def __init__(self, event_reader reader, int run_id):
         self.er = reader
         self._run_id = run_id
+        
     def __iter__(self):
         self.i = -1
         return self
+        
     def __next__(self):
         self.i+=1
         if self.i < self.er.er.numberOfEvents(self._run_id):
             return self[self.i]
         else:
             raise StopIteration
+            
     def __len__(self):
         return self.er.er.numberOfEvents(self._run_id)
+        
     def __getitem__(self, i):
         cdef int ii, start, stop
         if type(i)==slice:
@@ -561,6 +622,7 @@ cdef class event_reader_run:
             e = event()
             e.set(self.er.er.item(self._run_id, ii))
             return e
+            
     cpdef int run_id(self):
         return self._run_id
     
