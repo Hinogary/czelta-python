a49b72650862ec5ef58367987d444a931e07e05b
Martin Quarda <hinogary@gmail.com>
2014-04-30 12:43:57 +0200

    updated station.c

--- station.c
+++ station.c
@@ -1,17 +1,18 @@
 /*
  * Author: Martin Quarda
  */
-
-#include "station.h"
-#include "math.h"
+#include <math.h>
 #include "station.h"
 
+czelta_station czelta_station_stations[256];
 #define stations czelta_station_stations
-czelta_station stations[256];
+
+short czelta_station_nullCorrection[3] = {0,0,0};
 #define nullCorrection czelta_station_nullCorrection
-short* nullCorrection[3] = {0,0,0};
+
+char czelta_station_activate[256];
 #define activate czelta_station_activate
-bool activate[256];
+
 
 czelta_station* czelta_station_getStationById(int id){
     if(stations[id].id == id)
@@ -20,16 +21,17 @@ czelta_station* czelta_station_getStationById(int id){
         return stations;
 };
 
-bool czelta_station_exist(int id){
+int czelta_station_exist(int id){
     return id>0 && id<256 && stations[id].id == id && activate[id];
 }
 
 czelta_station* czelta_station_create(int id){
-    if(czelta_station_exist(&stations[id]))
-        czelta_station_clear(id);
+    if(czelta_station_exist(id))
+        czelta_station_delete(id);
     czelta_station_init(&stations[id]);
     stations[id].id = id;
-    activate[id] = true;
+    activate[id] = 1;
+    return &stations[id];
 }
 
 void czelta_station_add(czelta_station *st){
@@ -40,30 +42,26 @@ void czelta_station_add(czelta_station *st){
 
 void czelta_station_delete(int id){
     if(czelta_station_exist(id))
-        czelta_station_clear(&stations[id])
-    active[id] = false;
+        czelta_station_clear(&stations[id]);
+    activate[id] = 0;
 }
 
 void czelta_station_init(czelta_station* st){
+    int i;
     st->name = NULL;
-    for(int i=0;i<3;i++)
+    for(i=0;i<3;i++)
         st->gps_position[i] = 0;
-    for(int i=0;i<4;i++)
+    for(i=0;i<4;i++)
         st->detector_pos[i] = 0;
-    st->file_names_capacity = 5;
-    st->file_names_size = 0;
-    st->file_names = malloc(st->file_names_capacity*sizeof(void*));
     st->corrections = NULL;
     st->corrections_size = 0;
     st->corrections_capacity = 0;
 }
 
 void czelta_station_clear(czelta_station* st){
+    int i;
     if(st->name != NULL)
         free(st->name);
-    for(int i=0;i<st->file_names_size;i++)
-        free(st->file_names[i])
-    free(st->file_names);
     czelta_station_clearTDCCorrect(st);
 }
 
@@ -71,9 +69,10 @@ void czelta_station_setName(czelta_station* st, char* name){
     if(st->name != NULL)
         free(st->name);
     int len = strlen(name);
-    st->name = malloc(len);
+    st->name = malloc(len+1);
     if(st->name != NULL)
         memcpy(st->name, name, len);
+    st->name[len] = '\0';
 }
 
 void czelta_station_setGPSPosition(czelta_station* st, double latitude, double longitude, double height){
@@ -89,22 +88,6 @@ void czelta_station_setDetectorPosition(czelta_station* st, float x1, float y1, 
     st->detector_pos[3] = y2;
 }
 
-void czelta_station_pushFileName(czelta_station* st, char* filename){
-    if(st->file_names_size >= st->file_names_capacity){
-        char* new_ptr = realloc(st->file_names, st->file_names_capacity*2);
-        if(new_ptr == NULL)
-            return;
-        st->file_names = new_ptr;
-        st->file_names_capacity*=2;
-    }
-    int len = strlen(filename);
-    char* copied_filename = malloc(len);
-    if(copied_filename == NULL)
-        return;
-    memcpy(coped_filename, filename, len);
-    st->file_names[st->file_names_size++] = copied_filename;
-}
-
 void czelta_station_clearTDCCorrect(czelta_station* st){
     if(st->corrections != NULL)
         free(st->corrections);
@@ -114,12 +97,14 @@ void czelta_station_clearTDCCorrect(czelta_station* st){
 }
 
 void czelta_station_pushTDCCorrect(czelta_station* st, time_t from, short tdc0, short tdc1, short tdc2){
-    if(st->correction == NULL || st->correction_capacity <= st->correction_size){
-        int new_capacity = st->correction_capacity*2;
+    if(st->corrections == NULL || st->corrections_capacity <= st->corrections_size){
+        TDCCorrection* new_ptr;
+        int new_capacity = st->corrections_capacity*2;
         new_capacity = new_capacity==0?5:new_capacity;
-        TDCCorrection* new_ptr = realloc(st->correction, sizeof(TDCCorrection)*new_capacity);
+        new_ptr = realloc(st->corrections, sizeof(TDCCorrection)*new_capacity);
         if(new_ptr == NULL)
             return;
+        st->corrections_capacity = new_capacity;
         st->corrections = new_ptr;
     }
     TDCCorrection c = {from, {tdc0, tdc1, tdc2}};
@@ -144,7 +129,8 @@ short* czelta_station_lastTDCCorrect(czelta_station* st){
 
 short* czelta_station_TDCCorrect(czelta_station* st, time_t t){
     int r = st->corrections_size - 1;
-    for(int i=r;i>=0;i--){
+    int i = r;
+    for(;i>=0;i--){
         if(st->corrections[i].from>time)
             r=i-1;
         else
--- station.h
+++ station.h
@@ -1,12 +1,8 @@
 /*
  * Author: Martin Quarda
  */
-#include <string>
-#include <vector>
-#include <stdlib.h>
-#include <bitset>
-
 #include "common_func.h"
+#include <time.h>
 
 #ifndef STATION_H
 #define	STATION_H
@@ -25,21 +21,18 @@ typedef struct{
     char* name;
     double gps_position[3];
     float detector_pos[4];
-    char** file_names;
-    int file_names_size;
-    int file_names_capacity;
     TDCCorrection* corrections;
     int corrections_size;
     int corrections_capacity;
 } czelta_station;
 
-extern czelta_station* czelta_station_stations;
-extern short* czelta_station_nullCorrection;
-extern bool* czelta_station_activate;
+extern czelta_station czelta_station_stations[256];
+extern short czelta_station_nullCorrection[3];
+extern char czelta_station_activate[256];
 
 czelta_station* czelta_station_getStationById(int id);
 
-bool czelta_station_exist(int id);
+int czelta_station_exist(int id);
 czelta_station* czelta_station_create(int id);
 void czelta_station_add(czelta_station *st);
 void czelta_station_delete(int id);
@@ -50,13 +43,12 @@ void czelta_station_clear(czelta_station* st);
 void czelta_station_setName(czelta_station* st, char* name);
 void czelta_station_setGPSPosition(czelta_station* st, double latitude, double longitude, double height);
 void czelta_station_setDetectorPosition(czelta_station* st, float x1, float y1, float x2, float y2);
-void czelta_station_pushFileName(czelta_station* st, char* filename);
 void czelta_station_clearTDCCorrect(czelta_station* st);
 void czelta_station_pushTDCCorrect(czelta_station* st, time_t from, short tdc0, short tdc1, short tdc2);
 double czelta_station_distance_beetwen(czelta_station* st1, czelta_station* st2);
 
 int czelta_station_id(czelta_station* st);
-char* czelta_station_name(czelta_station* st);
+extern char* czelta_station_name(czelta_station* st);
 short* czelta_station_lastTDCCorrect(czelta_station* st);
 short* czelta_station_TDCCorrect(czelta_station* st, time_t t);
 double* czelta_station_GPSPosition(czelta_station* st);
@@ -65,16 +57,22 @@ float* czelta_station_detectorPosition(czelta_station* st);
 }
 
 #include <string>
+#include <vector>
 
 class Station {
 public:
+    inline Station(){
+        czelta_station_init(&st);
+    }
     inline Station(int ID){
         czelta_station_init(&st);
-        st->id = ID;
+        st.id = ID;
     };
-    inline ~Station(){czelta_station_clear(&st);};
+    inline ~Station(){
+        czelta_station_clear(&st);
+    };
     inline short* lastTDCCorrect(){
-        return czelta_station_lastTDCCorrect(st);
+        return czelta_station_lastTDCCorrect(&st);
     };
     inline short* TDCCorrect(time_t time){
         return czelta_station_TDCCorrect(&st, time);
@@ -84,7 +82,7 @@ public:
     };
     //latitude, longtitude, height
     inline double* GPSPosition(){
-        czelta_station_GPSPosition(&st);
+        return czelta_station_GPSPosition(&st);
     };
     inline const char* name(){
         return czelta_station_name(&st);
@@ -96,44 +94,50 @@ public:
         return czelta_station_distance_beetwen(&this->st, &st.st);
     };
 
-    void setName(char* name){
+    inline void setName(char* name){
         czelta_station_setName(&st, name);
     };
-    void setGPSPosition(double latitude, double longitude, double height){
+    inline void setGPSPosition(double latitude, double longitude, double height){
         czelta_station_setGPSPosition(&st, latitude, longitude, height);
     };
-    void setDetectorPosition(float x1, float y1, float x2, float y2){
+    inline void setDetectorPosition(float x1, float y1, float x2, float y2){
         czelta_station_setDetectorPosition(&st, x1, y1, x2, y2);
     };
-    void clearTDCCorrect(){
+    inline void clearTDCCorrect(){
         czelta_station_clearTDCCorrect(&st);
     };
-    void pushTDCCorrect(time_t from, short tdc0, short tdc1, short tdc2){
+    inline void pushTDCCorrect(time_t from, short tdc0, short tdc1, short tdc2){
         czelta_station_pushTDCCorrect(&st, from, tdc0, tdc1, tdc2);
     };
-    void pushTDCCorrect(std::string from, short tdc0, short tdc1, short tdc2){
-        czelta_station_push_TDCCorrect(&st, string_date(from), tdc0, tdc1, tdc2);
-    };
-    void pushFileName(string name){
-        czelta_station_pushFileName(&st, name.c_str());
+    inline void pushTDCCorrect(std::string from, short tdc0, short tdc1, short tdc2){
+        czelta_station_pushTDCCorrect(&st, string_date(from), tdc0, tdc1, tdc2);
     };
     
     //static methods
-    static Station& getStation(uint8_t ID){
+    inline static Station& getStation(uint8_t ID){
         return *(Station*)&czelta_station_stations[ID];
     };
     
-    static Station& getStation(string name){
-        
+    inline static Station& getStation(std::string name){
+        const char* name_char = name.c_str();
+        for(int i=1;i<256;i++){
+            if(czelta_station_name(&czelta_station_stations[i]) != NULL && !strcmp(name_char, czelta_station_name(&czelta_station_stations[i])))
+                return *(Station*)&czelta_station_stations[i];
+        }
+        return *(Station*)&czelta_station_stations[0];
     };
     
-    static vector<Station*> getStations(){
-        
+    inline static std::vector<Station*> getStations(){
+        std::vector<Station*> sts;
+        for(int i=1;i<256;i++)
+            if(czelta_station_stations[i].id == i)
+                sts.push_back((Station*)&czelta_station_stations[i]);
+        return sts;
     };
     
     //return false if added
-    inline static bool addStation(Station station){
-        czelta_station_add(&st);
+    inline static void addStation(Station station){
+        czelta_station_add(&station.st);
     };
     
     inline static bool active(uint8_t index){
