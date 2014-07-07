90f6a89b77a4ff84a5371dba2444ab813b08b3b2
Martin Quarda <hinogary@gmail.com>
2014-07-07 11:03:44 +0200

    added libnova dependency

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ ext_modules = [Extension('czelta', ['czelta.pyx' if USE_CYTHON else 'czelta.cpp'
                                     'station.c',
                                     'coincidence.cpp'],
                 language='c++',
-                extra_compile_args=['-std=c++0x']
+                extra_compile_args=['-std=c++0x', '-fopenmp','-I/usr/include/libnova'],
+                extra_link_args=['-fopenmp','-lnova']
     )]
 if USE_CYTHON:
     ext_modules = cythonize(ext_modules)
